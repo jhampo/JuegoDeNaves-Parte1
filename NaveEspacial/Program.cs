c748e91c71cba91c418d593f88c6e35c591df943
Usuario <Usuario@LAPTOP-QCC5M855>
2023-01-29 13:30:16 -0500

    Add project files.

---
+++ NaveEspacial/Program.cs
@@ -0,0 +1,6 @@
+﻿using NaveEspacial;
+using System.Drawing;
+
+Ventana ventana = new Ventana(170, 45, ConsoleColor.Black, new Point(5, 3), new Point(165, 43));
+ventana.DibujarMarco();
+Console.ReadKey();  
